25605da43d1324ddf559f144d58f7781b1e7eae7
tkerskerVCA <108771809+tkerskerVCA@users.noreply.github.com>
2023-01-09 12:12:42 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+let projectile: Sprite = null
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, randint(-90, -30), 50)
+    projectile.y = randint(0, scene.screenHeight())
+    projectile.setFlag(SpriteFlag.Ghost, true)
+})
