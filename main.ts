274c90e8ff4a970a1f7d8f3e46cbe3eb7304a1d2
tkerskerVCA <108771809+tkerskerVCA@users.noreply.github.com>
2024-01-16 11:15:23 -0600

    Added a meteor and On Sprite Overlap with projectile (#1)

--- main.ts
+++ main.ts
@@ -1,4 +1,26 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprites.destroy(sprite, effects.fire, 500)
+})
 let projectile: Sprite = null
+let mySprite = sprites.create(img`
+    . . . . . . . . . c c 8 . . . . 
+    . . . . . . 8 c c c f 8 c c . . 
+    . . . c c 8 8 f c a f f f c c . 
+    . . c c c f f f c a a f f c c c 
+    8 c c c f f f f c c a a c 8 c c 
+    c c c b f f f 8 a c c a a a c c 
+    c a a b b 8 a b c c c c c c c c 
+    a f c a a b b a c c c c c f f c 
+    a 8 f c a a c c a c a c f f f c 
+    c a 8 a a c c c c a a f f f 8 a 
+    . a c a a c f f a a b 8 f f c a 
+    . . c c b a f f f a b b c c 6 c 
+    . . . c b b a f f 6 6 a b 6 c . 
+    . . . c c b b b 6 6 a c c c c . 
+    . . . . c c a b b c c c . . . . 
+    . . . . . c c c c c c . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
 game.onUpdateInterval(500, function () {
     projectile = sprites.createProjectileFromSide(img`
         . . . . . . . . . . . . . . . . 
@@ -19,5 +41,5 @@ game.onUpdateInterval(500, function () {
         . . . . . . . . . . . . . . . . 
         `, randint(-90, -30), 50)
     projectile.y = randint(0, scene.screenHeight())
-    projectile.setFlag(SpriteFlag.Ghost, true)
+    projectile.setFlag(SpriteFlag.Ghost, false)
 })
